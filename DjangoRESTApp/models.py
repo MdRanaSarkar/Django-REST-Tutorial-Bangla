02a8356d5433e358413cd95077ce5b4a23e9a531
MdRanaSarkar <ranasarkar1503031@gmail.com>
2020-11-15 07:52:17 +0600

    first commit

---
+++ DjangoRESTApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    authorname = models.CharField(max_length=200)
+    email = models.EmailField(max_length=20)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
---
+++ DjangoRESTApp/serializers.py
@@ -0,0 +1,29 @@
+from DjangoRESTApp.models import Article
+from rest_framework import serializers
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    # Model serializer
+    class Meta:
+        model = Article
+        fields = ['title', 'authorname', 'email', 'date']
+
+    """
+    #Normal serializer
+    title = serializers.CharField(max_length=200)
+    authorname = serializers.CharField(max_length=200)
+    email = serializers.EmailField(max_length=20)
+    date = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Article.objects.create(validated_data)
+
+    def update(self, instance, validated_datas):
+        instance.title = validated_datas.get('title', instance.title)
+        instance.authorname = validated_datas.get('authorname', instance.authorname)
+        instance.email = validated_datas.get('email', instance.email)
+        instance.date = validated_datas.get('date', instance.date)
+        instance.save()
+        return instance
+
+    """
